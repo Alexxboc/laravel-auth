07b12732e142e6fbfa6b8089075e76c4541fa4f0
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-22 13:52:41 +0200

    Fix views style

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import App from './views/App';
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
-    .vue({ version: 2 })
+    .js("resources/js/admin.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/admin.scss", "public/css")
     .copyDirectory("resources/img", "public/img")
