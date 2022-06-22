efa1548cbb5e447a06fa1505ec1b3d44f180b2a5
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-22 14:21:39 +0200

    - Return view edit in PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -72,7 +72,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
